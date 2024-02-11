9aa6e8f46a10de972e63aff6ac2bb3dc5ae7932c
Gabi Farias <gabijk1234row@gmail.com>
2024-02-10 20:59:58 -0400

    [UPDATE] pizza graphic

--- src/composable/PercentageView.vue
+++ src/composable/PercentageView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="h-96 w-auto bg-light-red border-4 border-solid border-secundary rounded-md flex justify-center">
+    <div class="bg-light-red flex justify-center p-4">
+      <PizzaGraphic :data="chartData" />
+    </div>
     <div class="flex flex-col items-center p-2">
       <h1 class="font-bold text-3xl flex justify-center pt-4 mb-16">
         Estados participantes:
@@ -14,35 +17,60 @@
       <div class="flex flex-row justify-center items-center font-medium text-2xl p-4">
         <h3>{{ resultMessage }}</h3>
       </div>
+      <div class="flex flex-row justify-center items-center font-medium text-2xl p-4">
+        <h3>{{ percentage }}</h3>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup>
-  import { ref } from 'vue'
-  import SpButton from '@/components/globals/buttons/SpButoon.vue'
-  import EsButton from '@/components/globals/buttons/EsButton.vue'
-  import MgButton from '@/components/globals/buttons/MgButton.vue'
-  import OthersButton from '@/components/globals/buttons/OthersButton.vue'
-  import RjButton from '@/components/globals/buttons/RjButton.vue'
-  import data from '../data/data.json'
+<script>
+import { ref } from 'vue'
+import data from '../data/data.json'
+import PizzaGraphic from '@/composable/PizzaGraphic.vue'
+import SpButton from '@/components/globals/buttons/SpButoon.vue'
+import EsButton from '@/components/globals/buttons/EsButton.vue'
+import MgButton from '@/components/globals/buttons/MgButton.vue'
+import RjButton from '@/components/globals/buttons/RjButton.vue'
+import OthersButton from '@/components/globals/buttons/OthersButton.vue'
 
-  const activeButton = ref(null)
-  const resultMessage = ref('')
-  const totalValue = ref(0)
+export default {
+  components: {
+    SpButton,
+    RjButton,
+    MgButton,
+    EsButton,
+    OthersButton,
+    PizzaGraphic
+  },
+  setup() {
+    const activeButton = ref(null)
+    const resultMessage = ref('')
+    const percentage = ref('')
+    const totalValue = data.reduce((acc, curr) => acc + curr.valor, 0)
+    const chartData = data.map(item => ({
+      estado: item.estado,
+      percentage: (item.valor / totalValue) * 100
+    }))
 
-  data.forEach(item => {
-    totalValue.value += item.valor
-  })
+    const handleButtonClick = (button) => {
+      activeButton.value = button
 
-  const handleButtonClick = (button) => {
-    activeButton.value = button
+      const selectedState = data.find(item => item.estado === button)
 
-    const selectedState = data.find(item => item.estado === button)
+      if (selectedState) {
+        const percentageValue = (selectedState.valor / totalValue) * 100
+        percentage.value = percentageValue.toFixed(2) + '%'
+        resultMessage.value = `${selectedState.estado} tem um valor de R$ ${selectedState.valor.toFixed(2)} do total de R$ ${totalValue.toFixed(2)}`
+      }
+    }
 
-    if (selectedState) {
-      const percentage = (selectedState.valor / totalValue.value) * 100
-      resultMessage.value = `${selectedState.estado} tem um valor de R$ ${selectedState.valor.toFixed(2)} do total de R$ ${totalValue.value.toFixed(2)}`
+    return {
+      resultMessage,
+      percentage,
+      chartData,
+      handleButtonClick
     }
   }
+}
 </script>
---
+++ src/composable/PizzaGraphic.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+      <canvas ref="chartCanvas"></canvas>
+    </div>
+  </template>
+  
+  <script>
+  import Chart from 'chart.js/auto'
+  
+  export default {
+    props: {
+      data: {
+        type: Array,
+        required: true
+      }
+    },
+    mounted() {
+      this.renderChart()
+    },
+    methods: {
+      renderChart() {
+        const ctx = this.$refs.chartCanvas.getContext('2d')
+  
+        new Chart(ctx, {
+          type: 'doughnut',
+          data: {
+            labels: this.data.map(item => item.estado),
+            datasets: [{
+              label: 'Percentage',
+              data: this.data.map(item => item.percentage.toFixed(2)),
+              backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(104,0,0,1)',
+                'rgba(177,17,22,1)',
+                'rgba(255,0,0,1)',
+                'rgba(139,3,4,1)',
+              ],
+              borderWidth: 1
+            }]
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: false
+          }
+        })
+      }
+    }
+  }
+  </script>
